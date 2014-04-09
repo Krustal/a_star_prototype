7471fb6bb6d8e4a2c93383df267f578b5cfa7f44
Byron McMullen <byronsm@gmail.com>
2014-04-08 19:43:33 -0700

    basic movement
    really bad camera but movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // FixedUpdate is called before physics updates
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+}
